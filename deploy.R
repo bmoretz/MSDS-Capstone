961a6fed48e7f12385381ac6d518c0ad78a6d4b4
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-11-02 17:22:26 -0400

    dashboard updates
    dashboard updates

--- deploy.R
+++ deploy.R
@@ -1,7 +1,5 @@
 library(rsconnect)
 
-rsconnect::deployApp('D:/Projects/MSDS-RiskAnalytics/PortfolioAnalytics/apps/portfolio_return_dist.rmd')
-rsconnect::deployApp('D:/Projects/MSDS-RiskAnalytics/PortfolioAnalytics/apps/portfolio_volatility.rmd')
-rsconnect::deployApp('D:/Projects/MSDS-RiskAnalytics/PortfolioAnalytics/apps/portfolio_skewness.rmd')
-rsconnect::deployApp('D:/Projects/MSDS-RiskAnalytics/PortfolioAnalytics/apps/commodity_predict.rmd')
+rsconnect::deployApp('D:/Projects/MSDS-Capstone/dashboard/QuantumCapital.Rmd')
+
 
